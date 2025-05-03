d3e40723a4fd3f9ef1e315ee6e58396e72c500b8
tifenum <boukadidahbib@gmail.com>
2025-05-03 18:52:17 +0100

    aa

--- src/app/hotel-details/page.tsx
+++ src/app/hotel-details/page.tsx
@@ -1,5 +1,5 @@
-"use client";
 export const dynamic = 'force-dynamic';
+"use client";
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { fetchFakeHotel, createBooking } from "@/services/hotelService";
