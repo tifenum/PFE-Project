86596e0e64f189ec36be16e1d87908444f500b3e
tifenum <boukadidahbib@gmail.com>
2025-05-03 19:02:07 +0100

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-export const dynamic = 'force-dynamic';
 "use client"
+export const dynamic = 'force-dynamic';
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Blog from "@/components/Blog";
