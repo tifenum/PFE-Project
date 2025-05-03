ee641adbd0756e94132b929af1f73851ee162fca
tifenum <boukadidahbib@gmail.com>
2025-05-03 18:43:55 +0100

    again

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  telemetry: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
--- src/app/hotel-details/page.tsx
+++ src/app/hotel-details/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = 'force-dynamic';
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { fetchFakeHotel, createBooking } from "@/services/hotelService";
