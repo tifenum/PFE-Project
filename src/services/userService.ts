7f52f76e4cd311c75519d6a0c797fb66881ee6b4
tifenum <boukadidahbib@gmail.com>
2025-05-06 19:10:53 +0100

    a

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { API_BASE_URL } from "./config";
 
-const API_BASE_URL1 = `/api/auth`;
+const API_BASE_URL1 = `${API_BASE_URL}/auth`;
 
 export const login = async (email: string, password: string) => {
   try {
