51de5877574b2c371219e7d81cc4357b2d702cea
tifenum <boukadidahbib@gmail.com>
2025-05-30 01:53:27 +0100

    v

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   const pathname = usePathname(); // Get current pathname
 const hideFooter = pathname === "/chatbot" || pathname === "/map";
 const hideHeader = pathname === "/map";
- // Get current pat
+ // Get current p
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
