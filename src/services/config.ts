381a34f00ce3bf2c9a6c1e511088711b6ee21619
tifenum <boukadidahbib@gmail.com>
2025-06-06 02:36:54 +0100

    fv

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,3 +1,3 @@
 // services/config.ts
-// export const API_BASE_URL = "/api";
-export const API_BASE_URL = "http://localhost:8222/api";  
+export const API_BASE_URL = "/api";
+// export const API_BASE_URL = "http://localhost:8222/api";
