b84771d08a5eaf950aacdb66c02006a140eb6165
tifenum <boukadidahbib@gmail.com>
2025-06-08 22:27:09 +0100

    bb

--- src/app/hotels/page.tsx
+++ src/app/hotels/page.tsx
@@ -1,13 +1,8 @@
 // app/blog/page.tsx
 export const dynamic = 'force-dynamic';
 
-import type { Metadata } from 'next'
 import ClientBookingPage from './BookingClient'
 
-export const metadata: Metadata = {
-  title: "Blog Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Blog Page for Startup Nextjs Template",
-}
 
 export default function BlogPage() {
   return <ClientBookingPage />
